d7ff30cd0ed076a48a24da17e1dbaaedbd61b467
Edwin Rodríguez <edwinrodriguez@Edwins-MacBook-Pro.local>
2015-05-23 16:16:38 -0400

    Setup the look of the album view controller.

---
+++ VirtualTourist/AlbumViewController.swift
@@ -0,0 +1,34 @@
+//
+//  AlbumViewController.swift
+//  VirtualTourist
+//
+//  Created by Edwin Rodriguez on 5/23/15.
+//  Copyright (c) 2015 Edwin Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class AlbumViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
